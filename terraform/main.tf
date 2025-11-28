7fe9517ec1f15cf44c326b26d2c24232e34c98af
srikanthmanam1 <166566889+srikanthmanam1@users.noreply.github.com>
2025-11-28 16:49:40 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@
 #------------------------------------------------------------
 
 # Create a VPC
-resource "aws_vpc" "main" {
+resource "aws_vpc" "sm_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support   = true	# To ensure to have Public DNS 
   enable_dns_hostnames = true	# To ensure to have Public DNS 
@@ -13,50 +13,50 @@ resource "aws_vpc" "main" {
 }
 
 # Create a subnet
-resource "aws_subnet" "main" {
-  vpc_id                  = aws_vpc.main.id
+resource "aws_subnet" "sm_subnet" {
+  vpc_id                  = aws_vpc.sm_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "${var.aws_region}a"
   map_public_ip_on_launch = true	# To ensure to have Public ip
 
   tags = {
-    Name = "sm-subnet"
+    Name = "sm-subnet1"
   }
 }
 
 # Create an internet gateway
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
+resource "aws_internet_gateway" "sm_igw" {
+  vpc_id = aws_vpc.sm_vpc.id
   tags = {
-    Name = "sm-igw"
+    Name = "sm-igw1"
   }
 }
 
 # Create a route table
-resource "aws_route_table" "rtable" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table" "sm_route_table" {
+  vpc_id = aws_vpc.sm_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
+    gateway_id = aws_internet_gateway.sm_igw.id
   }
 
   tags = {
-    Name = "sm-route-table"
+    Name = "sm-route-table1"
   }
 }
 
 # Associate subnet with route table
-resource "aws_route_table_association" "assoc" {
-  subnet_id      = aws_subnet.main.id
-  route_table_id = aws_route_table.rtable.id
+resource "aws_route_table_association" "sm_rt_association" {
+  subnet_id      = aws_subnet.sm_subnet.id
+  route_table_id = aws_route_table.sm_route_table.id
 }
 
 # Create a security group
-resource "aws_security_group" "allow_ssh" {
-  name        = "sm-sg_ssh_http_s" # security group name
+resource "aws_security_group" "sm_sg" {
+  name        = "sm-sg1_ssh_http_s" # security group name
   description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = aws_vpc.sm_vpc.id
 
   ingress {
     description = "SSH"
@@ -88,15 +88,15 @@ resource "aws_security_group" "allow_ssh" {
   }
 
   tags = {
-    Name = "sm-sg_ssh_http_s2" # name of sg tag
+    Name = "sm-sg1-tag_ssh_http_s" # name of sg tag
   }
 }
 
 ###############################
 # IAM Role
 ###############################
-resource "aws_iam_role" "ec2_role" {
-  name = "sm-ec2-create-role"
+resource "aws_iam_role" "sm_ec2_role" {
+  name = "sm-ec2-role1"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -113,9 +113,9 @@ resource "aws_iam_role" "ec2_role" {
 ###############################
 # IAM Policy (EC2 + VPC Create)
 ###############################
-#resource "aws_iam_policy" "ec2_create_policy" {
-#  name        = "sm-ec2-create-policy"
-#  description = "Allow EC2 instance to create/manage EC2 resources including VPC, subnets, IGW, route t>
+#resource "aws_iam_policy" "sm_ec2_policy" {
+#  name        = "sm-ec2-policy1"
+#  description = "Allow EC2 instance to create/manage EC2 resources including VPC, subnets, IGW, route tables"
 
 #  policy = jsonencode({
 #    Version = "2012-10-17"
@@ -169,8 +169,8 @@ resource "aws_iam_role" "ec2_role" {
 #  })
 #}
 
-resource "aws_iam_policy" "ec2_create_policy" {
-  name        = "sm-ec2-create-policy"
+resource "aws_iam_policy" "sm_ec2_policy" {
+  name        = "sm-ec2-policy1"
   description = "Full EC2 access"
   policy = jsonencode({
     Version = "2012-10-17"
@@ -183,36 +183,35 @@ resource "aws_iam_policy" "ec2_create_policy" {
     ]
   })
 }
-
 ###############################
 # Attach Policy to Role
 ###############################
-resource "aws_iam_role_policy_attachment" "attach_ec2_create" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = aws_iam_policy.ec2_create_policy.arn
+resource "aws_iam_role_policy_attachment" "sm_attach_ec2_role_policy" {
+  role       = aws_iam_role.sm_ec2_role.name
+  policy_arn = aws_iam_policy.sm_ec2_policy.arn
 }
 
 ###############################
 # Instance Profile
 ###############################
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "sm-ec2-create-profile"
-  role = aws_iam_role.ec2_role.name
+resource "aws_iam_instance_profile" "sm_ec2_instance_profile" {
+  name = "sm-ec2-instance-profile1"
+  role = aws_iam_role.sm_ec2_role.name
 }
 
 # Create an EC2 instance
-resource "aws_instance" "web" {
+resource "aws_instance" "sm_ec2_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  subnet_id     = aws_subnet.main.id
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  subnet_id     = aws_subnet.sm_subnet.id
+  vpc_security_group_ids = [aws_security_group.sm_sg.id]
   #associate_public_ip_address = true  # <--- Ensures a public IP is assigned for instance. Overrides subnet setting
-  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+  iam_instance_profile = aws_iam_instance_profile.sm_ec2_instance_profile.name
 
-  key_name      = var.key_name
+  #key_name      = var.key_name
   #user_data = file("sm0_update.sh")
   tags = {
-    Name = "sm-tf-ec2"
+    Name = "sm-ec2-tf-ex1"
   }
 }
 #https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_manage_delete.html
