588edd9110dae6234cc999b46eb8eaa0107c0bc1
srikanthmanam1 <166566889+srikanthmanam1@users.noreply.github.com>
2025-11-28 21:31:57 -0600

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,8 +8,8 @@ variable "aws_region" {
 
 variable "instance_type" {
   description = "EC2 instance type"
-//  default     = "t3.large"
-  default     = "t2.micro"
+  default     = "t3.large"
+//  default     = "t2.micro"
 }
 
 variable "ami_id" {
